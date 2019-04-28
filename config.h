a1a44091907cf1dcc15cae238f93c7e075b84ec9
Felipe Silva <felipe.dacs@gmail.com>
2019-04-28 15:42:17 -0300

    Add weather, calcurse e rem ranger

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -64,64 +64,67 @@ static const char *qutebrowser[]  = { "qutebrowser", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
+	/* modifier             key        function        argument */
     // close
-	{ MODKEY|ShiftMask,             XK_e,       quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,       killclient,     {0} },
+	{ MODKEY|ShiftMask,     XK_e,       quit,           {0} },
+	{ MODKEY|ShiftMask,     XK_q,       killclient,     {0} },
+    { MODKEY,               XK_l,       spawn,          SHCMD("st -e i3lock") },
+    { MODKEY|ShiftMask,     XK_l,       spawn,          SHCMD("st -e sudo shutdown -h 0") },
 
     //spawn softwares
-	{ MODKEY,                       XK_d,       spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,  spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,       spawn,          {.v = qutebrowser } },
-    { MODKEY,                       XK_r,       spawn,          SHCMD("st -e ranger") },
-    { MODKEY,                       XK_n,       spawn,          SHCMD("st -e nnn") },
+	{ MODKEY,               XK_d,       spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,     XK_Return,  spawn,          {.v = termcmd } },
+    { MODKEY,               XK_b,       spawn,          {.v = qutebrowser } },
+    { MODKEY,               XK_n,       spawn,          SHCMD("st -e nnn") },
+    { MODKEY,               XK_t,       spawn,          SHCMD("st -e calcurse") },
+    { MODKEY,               XK_t,       spawn,          SHCMD("st -e cal -y") },
 
     //soud volume
-//{ MODKEY,                       XK_plus,    spawn,          {.v = st -e nnn } },
-//{ MODKEY|ShiftMask,             XK_plus,    spawn,          {.v = st -e nnn } },
-//{ MODKEY,                       XK_minus,   spawn,          {.v = st -e nnn } },
-//{ MODKEY|ShiftMask,             XK_minus,   spawn,          {.v = st -e nnn } },
-//{ MODKEY,                       XK_equal,   spawn,          {.v = st -e nnn } },
+    //{ MODKEY,              XK_plus,    spawn,          {.v = st -e nnn } },
+    //{ MODKEY|ShiftMask,    XK_plus,    spawn,          {.v = st -e nnn } },
+    //{ MODKEY,              XK_minus,   spawn,          {.v = st -e nnn } },
+    //{ MODKEY|ShiftMask,    XK_minus,   spawn,          {.v = st -e nnn } },
+    //{ MODKEY,              XK_equal,   spawn,          {.v = st -e nnn } },
 
-	{ MODKEY|ShiftMask,             XK_b,       togglebar,      {0} },
+	{ MODKEY|ShiftMask,     XK_b,       togglebar,      {0} },
 
     //resize
-	{ MODKEY,                       XK_h,       setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,       setmfact,       {.f = +0.05} },
+	{ MODKEY,               XK_h,       setmfact,       {.f = -0.05} },
+	{ MODKEY,               XK_l,       setmfact,       {.f = +0.05} },
 
     //movement windows
-	{ MODKEY,                       XK_Return,  zoom,           {0} },
-	{ MODKEY,                       XK_i,       incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_i,       incnmaster,     {.i = -1 } },
+	{ MODKEY,               XK_Return,  zoom,           {0} },
+	{ MODKEY,               XK_i,       incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_i,       incnmaster,     {.i = -1 } },
 
     //layouts
-	{ MODKEY,                       XK_t,       setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,       setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,       setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,   setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,   togglefloating, {0} },
+	{ MODKEY,               XK_t,       setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,               XK_f,       setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,               XK_m,       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,               XK_space,   setlayout,      {0} },
+	{ MODKEY|ShiftMask,     XK_space,   togglefloating, {0} },
 
     // ?????
-	{ MODKEY,                       XK_c,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_c,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_z,       focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_x,       focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_z,       tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_x,       tagmon,         {.i = +1 } },
+	{ MODKEY,               XK_k,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,     XK_k,      tag,            {.ui = ~0 } },
+	{ MODKEY,               XK_z,       focusmon,       {.i = -1 } },
+	{ MODKEY,               XK_x,       focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_z,       tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_x,       tagmon,         {.i = +1 } },
 
     // movement
-	{ MODKEY,                       XK_j,       focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,       focusstack,     {.i = -1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY,                       XK_Tab,     view,           {0} },
+	{ MODKEY,               XK_j,       focusstack,     {.i = +1 } },
+	{ MODKEY,               XK_k,       focusstack,     {.i = -1 } },
+	TAGKEYS(                XK_1,                      0)
+	TAGKEYS(                XK_2,                      1)
+	TAGKEYS(                XK_3,                      2)
+	TAGKEYS(                XK_4,                      3)
+	TAGKEYS(                XK_5,                      4)
+	TAGKEYS(                XK_6,                      5)
+	TAGKEYS(                XK_7,                      6)
+	TAGKEYS(                XK_8,                      7)
+	TAGKEYS(                XK_9,                      8)
+	{ MODKEY,               XK_Tab,     view,           {0} },
 };
 
 /* button definitions */
