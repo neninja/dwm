73bde33bcc6a8dedd2d0e87798cb074aec8571d4
Felipe Silva <felipe.dacs@gmail.com>
2019-05-02 21:35:28 -0300

    Qutebrowser in 7

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	 */
 	/* class            instance    title       tags mask     isfloating   monitor */
 	//{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
-	{ "qutebrowser",    NULL,       NULL,       1<<8,         1,           -1 },
+	{ "qutebrowser",    NULL,       NULL,       1<<6,         1,           -1 },
 };
 
 /* layout(s) */
