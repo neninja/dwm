82c959b49ed935554bdd4c89bfab534b52e682da
Felipe Silva <felipe.dacs@gmail.com>
2019-05-03 23:04:11 -0300

    Correção da tag emoji para 6

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 //static const char *fonts[]          = { "monospace:size=10" };
-static const char *fonts[]          = { "UbuntuMono Nerd Font:size=10" };
+static const char *fonts[]          = { "Noto Color Emoji:size=10", "UbuntuMono Nerd Font:size=10"};
 //static const char dmenufont[]       = "monospace:size=10";
 static const char dmenufont[]       = "UbuntuMono Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
@@ -34,7 +34,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "😣", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
